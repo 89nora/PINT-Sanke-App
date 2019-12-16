8b24c54c32238163c10545649f7e49098976bc84
machinegunjoeviterbo <m@MacBook-Air.local>
2019-12-16 18:09:46 +0100

    Move InterestScreen to screens and update App.js

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import { createStackNavigator } from 'react-navigation-stack';
 import { Image, StyleSheet, View, Text } from 'react-native';
 import MapScreen from './screens/MapScreen.js';
 import CameraScreen from './screens/CameraScreen.js';
-import InterestScreen from './InterestScreen.js';
+import InterestScreen from './screens/InterestScreen.js';
 
 //TAB NAVIGATOR - THIS SCRIPT NOW BELONGS TO REACT-NAVIGATION-TABS
 
