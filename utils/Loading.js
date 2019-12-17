175c389b86152c2e5abf1b5355cfdc50ba65dc3d
machinegunjoeviterbo <m@MacBook-Air.local>
2019-12-17 11:27:32 +0100

    Move around files like a bozz ;)

--- utils/Loading.js
+++ utils/Loading.js
@@ -1,6 +1,7 @@
 export default class Loading {
     //load method takes a callback as an argument. So when it executes setTimeout it waits 3 seconds and then fires
     //a setState function from startScreen component's constructor
+    //YEAH
     static load(callback) {
         setTimeout(callback, 2000);
     }
